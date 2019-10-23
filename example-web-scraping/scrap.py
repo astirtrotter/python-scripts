099b37549eb526edae26c99b94dfd87d2a210d11
Pro Dev <prodev9999@gmail.com>
2019-10-24 01:07:39 +0800

    Updated web-scraping without launching browser window by requests

--- example-web-scraping/scrap.py
+++ example-web-scraping/scrap.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+url = "https://www.amazon.com/b?node=18505469011&pf_rd_p=27643bbf-38ff-4db5-8032-83558fc1281c&pf_rd_r=KVXWW63EKNPN8QE90WH0"
+
+# using selenium's webdriver
+# this will launch a browser window
+# driver = webdriver.Firefox()
+# driver.get(url)
+# content = driver.page_source
+
+# using requests
+page = requests.get(url)
+content = page.text
 
 products = []
 hrefs = []
 prices = []
 ratings = []
 
-driver = webdriver.Firefox()
-driver.get("https://www.amazon.com/b?node=18505469011&pf_rd_p=27643bbf-38ff-4db5-8032-83558fc1281c&pf_rd_r=KVXWW63EKNPN8QE90WH0")
-content = driver.page_source
-
 soup = BeautifulSoup(content, 'html.parser')
 ul = soup.ul
-for li in ul.findAll('li', attrs={'class': 'celwidget'}):
+for li in ul.find_all('li', attrs={'class': 'celwidget'}):
   name = li.h2.text
   href = li.h2.parent.get('href')
   print(href)
